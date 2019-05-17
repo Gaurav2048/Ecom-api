3b8a58d626f36de84cf6b5f6902e97f55fc5e8ab
gaurav2048 <gunjankalita836@gmail.com>
2019-05-17 17:01:30 +0530

    Heruko related changes in env files

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,23 +2,30 @@ const env = require('./env');
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(env.database,
-                                env.username,
-                                env.password,
-                                {
-                                    host: env.host,
-                                    dialect:env.dialect,
-                                    OperatorsAliases:false,
+// const sequelize = new Sequelize(env.database,
+//                                 env.username,
+//                                 env.password,
+//                                 {
+//                                     host: env.host,
+//                                     dialect:env.dialect,
+//                                     OperatorsAliases:false,
 
-                                    pool:{
-                                        max:env.pool.max,
-                                        min: env.pool.min,
-                                        acquire: env.pool.acquire,
-                                        idle: env.pool.idle
-                                    }
-                                }
-    ); 
-    
+//                                     pool:{
+//                                         max:env.pool.max,
+//                                         min: env.pool.min,
+//                                         acquire: env.pool.acquire,
+//                                         idle: env.pool.idle
+//                                     }
+//                                 }
+//     ); 
+
+const sequelize = new Sequelize(env.heruko, {
+        dialect:env.dialect
+    }
+); 
+
+
+
     const db = {}; 
 
     db.Sequelize = Sequelize;
--- app/config/env.js
+++ app/config/env.js
@@ -1,7 +1,8 @@
 const env = {
-    database: 'mysql://b7d9cd89717728:6e88f948@us-cdbr-iron-east-02.cleardb.net/heroku_49b205d9e226867?reconnect=true',
+    database: 'Ecom-api',
     username: 'b7d9cd89717728',// root
     password: '6e88f948',
+    heroku: 'mysql://b7d9cd89717728:6e88f948@us-cdbr-iron-east-02.cleardb.net/heroku_49b205d9e226867?reconnect=true',
     host: 'localhost',
     dialect: 'mysql',
     pool: {
